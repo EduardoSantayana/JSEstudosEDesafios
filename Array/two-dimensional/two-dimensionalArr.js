57be173266c2e9f2818cda4589fbc9f7ae5a6589
Eduardo Santana <eduardoc.santayana@gmail.com>
2023-11-09 12:03:47 -0300

    Atualização do arquivo two-dimensional, correção e complementações.A função createArr2D define um array bidimensional. A função isSquareMatrix verifica se os arrays criados são matrizes quadráticas ou não.

--- Array/two-dimensional/two-dimensionalArr.js
+++ Array/two-dimensional/two-dimensionalArr.js
@@ -10,31 +10,59 @@ Anotações :
 Um array (matriz) vazio é simplesmente um array que não contém elementos, tendo sua dimensão igual 0x0.
 Uma matriz que tenha 1x1, um elemento em uma linha em uma coluna, tem ao menos um elemento.
 Exemplo: [[1]].
+
+
+#Revisa
 */
 
 
-function createArr2D(row, column){
-let Arr2D = [];    
-for (let i = 0; i < row; i++){
-    let row = [];
-for (let j = 0; j < column; j++) {
-    row.push(0);
-} Arr2D.push(row); 
-}
-return Arr2D;
+function createArr2D(row, column) {
+    let Arr2D = [];
+    for (let i = 0; i < row; i++) {
+        let row = []; // linha da matriz 
+        for (let j = 0; j < column; j++) {
+            row.push(0);// coluna da matriz
+        } Arr2D.push(row);
+    }
+    return Arr2D;
 }
 
 
-const minhaArr = createArr2D(4,4);
-console.log(minhaArr);
+const minhaArr = createArr2D(4, 4);
+//console.log(minhaArr);
 
 
 // somente com arrays. O array não pode ser vazio. 
 function isSquareMatrix(arr) {
     // verificar se é uma matriz quadrática.
-    // Lembrando que, uma matriz quadrática é aquela que possuir o número de linhas igual ao número colunas. Row = column.
-if (Array.isArray(arr) $$ arr.length > 0) {    
+    // Lembrando que, uma matriz quadrática é aquela que possuir o número de linhas igual ao número colunas. Row = column. 
+
+
+    if (Array.isArray(arr) && arr.length > 0) {
+        // Agora verificar as dimensões do array.
+        for (let index = 0; index < arr.length; index++) {
+            //  console.log("Row" + index);
+            // console.log("Colum" arr[index][número do elemento na coluna])/
+
+            // dimensions.push("linhas:"+ arr.length + "colunas:" + arr[index].length)
+            if (arr.length !== arr[index].length) {
+
+                console.log(`Não é quadrática. Dimensões: row ${ arr.length }, column: ${arr[index].length}`);
+                return false;
+            }
+        }
+
+        const numLinhas = arr.length;
+        const numColunas = arr[0].length;
+        let dimensions = ["Row:" + numLinhas, "column:" + numColunas]
+        
+        console.log(`É quadrática dimensions ${dimensions}`);
+        return dimensions;
+
+    } else {
+        console.error("parâmetro inválido");
+        return false;
+    }
 
 }
 
-}
